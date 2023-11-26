412bd8b7f60bcb36f3f80b8030d2c6421f9f91a1
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-11-26 11:56:20 +0100

    added auth0 authentication
    installed sdk,
    added api route for auth0,
    wrapped app in auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,8 @@ import ChecklistIcon from "@mui/icons-material/Checklist";
 import SettingsIcon from "@mui/icons-material/Settings";
 import LogoutIcon from "@mui/icons-material/Logout";
 import Diversity3Icon from "@mui/icons-material/Diversity3";
+// auth
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 import {
   AppBar,
@@ -46,72 +48,74 @@ const PLACEHOLDER_LINKS = [
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={{}}>
-      <AppBar position="fixed" sx={{ zIndex: 2000, background: "#4caf50" }}>
-        <Toolbar sx={{ backgroundColor: "background.paper" }}>
-          <Diversity3Icon
-            sx={{ color: "#444", mr: 2, transform: "translateY(-2px)" }}
-          />
-          <Typography variant="h6" noWrap component="div" color="black">
-            Friends Club
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: DRAWER_WIDTH,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
+      <UserProvider>
+        <AppBar position="fixed" sx={{ zIndex: 2000, background: "#4caf50" }}>
+          <Toolbar sx={{ backgroundColor: "background.paper" }}>
+            <Diversity3Icon
+              sx={{ color: "#444", mr: 2, transform: "translateY(-2px)" }}
+            />
+            <Typography variant="h6" noWrap component="div" color="black">
+              Friends Club
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          sx={{
             width: DRAWER_WIDTH,
-            boxSizing: "border-box",
-            top: ["48px", "56px", "64px"],
-            height: "auto",
-            bottom: 0,
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Divider />
-        <List>
-          {LINKS.map(({ text, href, icon: Icon }) => (
-            <ListItem key={href} disablePadding>
-              <ListItemButton component={Link} href={href}>
-                <ListItemIcon>
-                  <Icon />
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider sx={{ mt: "auto" }} />
-        <List>
-          {PLACEHOLDER_LINKS.map(({ text, icon: Icon }) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <Icon />
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          bgcolor: "background.default",
-          ml: `${DRAWER_WIDTH}px`,
-          mt: ["48px", "56px", "64px"],
-          p: 3,
-        }}
-      >
-        <ReduxProvider>
-          <Component {...pageProps} />
-        </ReduxProvider>
-      </Box>
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: DRAWER_WIDTH,
+              boxSizing: "border-box",
+              top: ["48px", "56px", "64px"],
+              height: "auto",
+              bottom: 0,
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <Divider />
+          <List>
+            {LINKS.map(({ text, href, icon: Icon }) => (
+              <ListItem key={href} disablePadding>
+                <ListItemButton component={Link} href={href}>
+                  <ListItemIcon>
+                    <Icon />
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider sx={{ mt: "auto" }} />
+          <List>
+            {PLACEHOLDER_LINKS.map(({ text, icon: Icon }) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Icon />
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            bgcolor: "background.default",
+            ml: `${DRAWER_WIDTH}px`,
+            mt: ["48px", "56px", "64px"],
+            p: 3,
+          }}
+        >
+          <ReduxProvider>
+            <Component {...pageProps} />
+          </ReduxProvider>
+        </Box>
+      </UserProvider>
     </ThemeProvider>
   );
 }
